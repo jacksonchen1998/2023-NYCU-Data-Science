b8e2a103d5be065ba743841709b22cfb6540e3e9
jacksonchen1998 <present90308@gmail.com>
2023-04-11 20:04:41 +0800

    [UPDATE] shell script

--- Homework 03/311511052.sh
+++ Homework 03/311511052.sh
@@ -1,5 +1,7 @@
-# conda install requirement.txt
-conda install --file requirements.txt
+# create conda environment by using the following command
+conda create --name 311511052 --file <this file>
+# activate conda environment
+conda activate 311511052
 # train model
 python train_model.py
 # finetune model
